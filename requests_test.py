ecd4d15f811b615e808bb6ad473d22e3a6afa9ae
IsisMoralesMaldonado <isis.moralesm@uanl.edu.mx>
2026-04-27 17:59:25 -0600

    quitar api key
    Quedé 🤡

--- requests_test.py
+++ requests_test.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 url = "https://serpapi.com/search.json"
-api_key = "82d2f92aa54bfc77bbf0fa2943ae653ad77a8dc78b638dd5a605a5b3af8c1c2b"
+api_key = " "
 
 bus_params = {
     "engine": "google_scholar",
