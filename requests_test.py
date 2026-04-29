573a8ed12ae0e8de02f0f3c181e2b17be14b18c3
IsisMoralesMaldonado <isis.moralesm@uanl.edu.mx>
2026-04-28 21:17:55 -0600

    crear carpeta si no existe
    Added directory creation for raw data storage if it doesn't exist.

--- requests_test.py
+++ requests_test.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 
 url = "https://serpapi.com/search.json"
 api_key = " "
@@ -8,7 +9,6 @@ bus_params = {
     "engine": "google_scholar",
     "q": "2001",
     "api_key": api_key,
-    "hl": "es"
 }
 #==================================================================
 try:
@@ -21,9 +21,11 @@ except Exception as fries:
   print("Error de conexión:", fries)
   
 #==============================================================
+if not os.path.exists("data/raw"):
+  os.makedirs("data/raw")
+    
 if response.status_code == 200:
   data = response.json()
-
     with open("data/raw/answer.json", "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
 else:
